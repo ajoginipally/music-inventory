b071bd72582518fa09b01c3ad333e865fabe1b65
ajoginipally <abhiteja@gmail.com>
2015-07-23 22:38:53 -0400

    added more edit functionality and added tests for filter

--- test/spec/filters/switchcase.js
+++ test/spec/filters/switchcase.js
@@ -1,19 +1,67 @@
 'use strict';
 
 describe('Filter: switchcase', function () {
+    var songs;
+    // load the filter's module
+    beforeEach(module('musicInventoryApp'));
 
-  // load the filter's module
-  beforeEach(module('musicInventoryApp'));
+    // initialize a new instance of the filter before each test
+    var switchcase;
+    beforeEach(inject(function ($filter) {
+        switchcase = $filter('switchcase');
+        songs = [
+            {
+                name: 'back in black',
+                genre: 'rock'
+            },
+            {
+                name: 'wide awake',
+                genre: 'pop'
+            },
+            {
+                name: 'money trees',
+                genre: 'rap'
+            }
+        ];
+    }));
 
-  // initialize a new instance of the filter before each test
-  var switchcase;
-  beforeEach(inject(function ($filter) {
-    switchcase = $filter('switchcase');
-  }));
+    /*it('filter rock songs should show a list of rock songs', inject(function (switchcase) {
+        var count = 0;
 
-  it('should return the input prefixed with "switchcase filter:"', function () {
-    var text = 'angularjs';
-    expect(switchcase(text)).toBe('switchcase filter: ' + text);
-  });
+        angular.forEach(switchcase('rock'), function (key, value) {
+            if (value.genre === 'rock') {
+                count++;
+            }
+        });
+
+        expect(count).toBe(switchcase('rock').length);
+    }));*/
+
+    it('filter rock songs should show a list of rock songs', function () {
+        var rockItems = switchcase(songs, 'rock');
+        expect(rockItems.length).toBe(1);
+    });
+
+    it('filter rap songs should show a list of rap songs', function () {
+        var rapItems = switchcase(songs, 'rap');
+        expect(rapItems.length).toBe(1);
+    });
+
+    it('filter pop songs should show a list of pop songs', function () {
+        var popItems = switchcase(songs, 'pop');
+        expect(popItems.length).toBe(1);
+    });
+
+    it('add another rock song to the list expect list to be two', function () {
+        var rockItems = switchcase(songs, 'rock');
+        expect(rockItems.length).toBe(1);
+        songs.push({
+            name: 'england',
+            genre: 'rock'
+        });
+        rockItems = switchcase(songs, 'rock');
+        expect(rockItems.length).toBe(2);
+
+    });
 
 });
